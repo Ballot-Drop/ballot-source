aa69d011fb095ce4cf3863891ccc9fdb10b7db0c
PJ Hoberman <phoberman@pocketoutdoormedia.com>
2020-08-24 10:25:55 -0600

    Ballot Source migration

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///ballot_source")}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///ballot-source")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
