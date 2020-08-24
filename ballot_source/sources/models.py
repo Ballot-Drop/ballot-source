eb1ba8109b5e3e743a991d3f219bea7a5036f410
PJ Hoberman <phoberman@pocketoutdoormedia.com>
2020-08-24 10:14:10 -0600

    Setting up models

--- ballot_source/sources/models.py
+++ ballot_source/sources/models.py
@@ -1,3 +1,40 @@
-# from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    SOURCE_TYPE_CHOICES = [
+        ("STATE", "State"),
+        ("COUNTY", "County"),
+        ("LOCATIONS", "Locations"),
+        ("OTHER", "Other"),
+    ]
+
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="sources_created",
+    )
+    updated = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        get_user_model(),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="sources_updated",
+    )
+    last_checked = models.DateTimeField(null=True, blank=True)
+    url = models.URLField()
+    source_type = models.CharField(
+        choices=SOURCE_TYPE_CHOICES, default="OTHER", max_length=20
+    )
+
+
+class SourceDetail(models.Model):
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name="details")
+    date_pulled = models.DateTimeField(auto_now_add=True)
+    raw_html = models.TextField()
+    diff = models.TextField()
